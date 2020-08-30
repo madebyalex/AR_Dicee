a37426da4ae663637052fd3ffaf46918f7b8963f
Alexander <hello@alexander.works>
2020-08-30 18:59:38 +0300

    Added a 3D model of a dice and some basic plane detection logic

--- AR_Dicee/ViewController.swift
+++ AR_Dicee/ViewController.swift
@@ -11,7 +11,7 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -20,25 +20,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-        
-        let cubeMaterial = SCNMaterial()
-        cubeMaterial.diffuse.contents = UIColor.red
-        cube.materials = [cubeMaterial]
-        
-        let cubeNode = SCNNode()
-        cubeNode.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
-        cubeNode.geometry = cube
-        
-        sceneView.scene.rootNode.addChildNode(cubeNode)
+        sceneView.autoenablesDefaultLighting = true
         
         
-
-//        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//
-//        // Set the scene to the view
-//        sceneView.scene = scene
+        // Create a new scene
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            diceNode.position = SCNVector3(x: 0, y: -0.03, z: -0.1)
+            sceneView.scene.rootNode.addChildNode(diceNode)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,8 +38,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
         
+        configuration.planeDetection = .horizontal
         
-
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -58,5 +49,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor {
+            print("Plane detected")
+        } else {
+            return
+        }
     }
 }
