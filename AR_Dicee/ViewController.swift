7122097f345919b1245917fbfec8aa1a7e189795
Alexander <hello@alexander.works>
2020-08-30 17:00:32 +0300

    Added an app icon

--- AR_Dicee/ViewController.swift
+++ AR_Dicee/ViewController.swift
@@ -20,11 +20,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        let cubeMaterial = SCNMaterial()
+        cubeMaterial.diffuse.contents = UIColor.red
+        cube.materials = [cubeMaterial]
+        
+        let cubeNode = SCNNode()
+        cubeNode.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        cubeNode.geometry = cube
+        
+        sceneView.scene.rootNode.addChildNode(cubeNode)
+        
+        
+
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -32,9 +46,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-         
-        print("World Tracking is supported = \(ARWorldTrackingConfiguration.isSupported)")
-        print("Session is supported = \(ARConfiguration.isSupported)")
+        
+        
 
         // Run the view's session
         sceneView.session.run(configuration)
